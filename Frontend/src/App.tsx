8719130eb1fdcd448e203764baf5d6e285cc0870
Rushi Katabathuni <115051948+rushykat@users.noreply.github.com>
2025-05-27 16:04:07 -0700

    storyboarding initial push

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import History from "./Components/History"
+
+
 const App = () => {
   return (
     <div>
       <h1>Shoot Yo' Shot</h1>
+      <p>Want to learn how to shoot a basketball?</p>
+      <p>Learn the history of good shooting form and how to shoot like the best in the game.</p>
+      <History />
     </div>
+
   )
 }
 
